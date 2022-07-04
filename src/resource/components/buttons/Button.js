b66e9f0f03a0ddf40c6406f68e94d39494e1e799
mi.tavakoli <mi.tavakoli@asiatech.ir>
2022-07-04 13:35:38 +0430

    add item options to buttons

--- src/resource/components/buttons/Button.js
+++ src/resource/components/buttons/Button.js
@@ -10,23 +10,41 @@ function Button({
   cancel,
   onClick,
   label,
+  iconLeft,
+  iconRight,
+  iconCenter,
+  styles,
   disabled = false,
   ...rest }) {
   return (
     <div className="">
       <button
-      className={`btn btn-${type} ${className} btn-${size}-size${
-        cancel ? " btn-cancel" : ""
-      } btn-disabled-${disabled}`}
-      onClick={(e) => {
-        !disabled && onClick && onClick(e);
-      }}
-      disabled={disabled}
-      {...rest}
-    >
-      {label}
-      {children}
-    </button>
+        className={`btn btn-${type} ${className} btn-${size}-size${cancel ? " btn-cancel" : ""
+          } btn-disabled-${disabled} btn-${styles}-styles`}
+        onClick={(e) => {
+          !disabled && onClick && onClick(e);
+        }}
+        disabled={disabled}
+        {...rest}
+      >
+        <div className="button-icon-wrapper">
+          {iconLeft && <div className="button-icon-item-wrapper">
+            <img src={iconLeft} className="button-icon-item" alt="button_icon" />
+          </div>
+          }
+          {iconRight && <div className="button-icon-item-wrapper">
+            <img src={iconRight} className="button-icon-item" alt="button_icon" />
+          </div>
+          }
+          {iconCenter &&
+            <div className="button-icon-center-item-wrapper">
+              <img src={iconCenter} className="button-icon-center-item" alt="button_icon" />
+            </div>
+          }
+          {label && <div>{label}</div>}
+        </div>
+        {children}
+      </button>
     </div>
   )
 }
@@ -44,6 +62,10 @@ Button.propTypes = {
   size: PropTypes.string,
   cancel: PropTypes.string,
   label: PropTypes.string,
+  iconLeft: PropTypes.string,
+  iconRight: PropTypes.string,
+  iconCenter: PropTypes.string,
+  styles: PropTypes.string,
 };
 
 Button.defaultProps = {
